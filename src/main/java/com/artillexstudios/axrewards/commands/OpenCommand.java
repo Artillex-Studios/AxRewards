78261a4657c673ea62b6cd74dc2e1e63e402f7a3
BenceX100 <birincsikbence@gmail.com>
2025-04-22 13:58:22 +0200

    3.4.2

--- src/main/java/com/artillexstudios/axrewards/commands/OpenCommand.java
+++ src/main/java/com/artillexstudios/axrewards/commands/OpenCommand.java
@@ -2,13 +2,15 @@ package com.artillexstudios.axrewards.commands;
 
 import com.artillexstudios.axrewards.commands.subcommands.Open;
 import com.artillexstudios.axrewards.guis.data.Menu;
+import com.artillexstudios.axrewards.guis.data.MenuManager;
+import org.bukkit.command.CommandException;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import revxrsal.commands.annotation.DefaultFor;
 import revxrsal.commands.orphan.OrphanCommand;
 
 public class OpenCommand implements OrphanCommand {
-    private final Menu menu;
+    private Menu menu;
 
     public OpenCommand(Menu menu) {
         this.menu = menu;
@@ -16,6 +18,10 @@ public class OpenCommand implements OrphanCommand {
 
     @DefaultFor({"~"})
     public void open(@NotNull Player sender) {
+        menu = MenuManager.getMenus().get(menu.name());
+        if (menu == null) {
+            throw new CommandException();
+        }
         Open.INSTANCE.execute(sender, menu);
     }
 }
